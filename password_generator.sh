46a76e9f5c9249d555559d4f88238df6ab412079
Md Towhidul Ahmed <63970469+towhidulahmed@users.noreply.github.com>
2026-01-22 23:49:44 +0100

    Clean up comments in password_generator.sh

--- password_generator.sh
+++ password_generator.sh
@@ -6,10 +6,6 @@ echo "This is a cryptography-based password generator"
 echo "Please enter the length of password: "
 read pass_len
 
-# Generate one password of requested length, 'seq 1' to 'seq N' if you want to generate N passwords
-# openssl rand -base64 48 generates 48 random bytes encoded in base64 (~64 chars)
-# cut trims output to desired length 'pass_len'
-
 for p in $(seq 1); do
     openssl rand -base64 48 | cut -c1-$pass_len
 done
